09ef6de8b06d2cdf5890c41de8a2d614dc40ffa3
claudiacazacu <claudia.cazacu29@gmail.com>
2026-01-12 03:33:28 +0200

    1
    Remove media files path addition in development mode.

--- numeproiect/urls.py
+++ numeproiect/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('', include('myapp.urls')),
 ]
 
-# Adăugăm calea către fișierele media în modul dezvoltare
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
